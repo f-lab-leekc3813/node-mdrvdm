01a1c6029740feac5896086f4df44eb404787c89
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2022-12-16 13:49:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-mdrvdm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-mdrvdm)
---
+++ index.js
@@ -0,0 +1,5 @@
+// run `node index.js` in the terminal
+var hour = 3
+var min = 3
+var sec = 3
+console.log(`현재 시각은 ${hour}시 ${min}분 ${sec}초 입니다.`)
